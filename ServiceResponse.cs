c6e97b8b37359e02c2c1cfc597d516a901fae9d0
vpp090 <vasil.petrov090@gmail.com>
2024-01-04 18:23:55 +0200

    first commit

---
+++ ServiceResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ServiceResponse
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+        public Exception Error { get; set; }
+
+        public ServiceResponse(T data)
+        {
+            Data = data;
+        }
+
+        public ServiceResponse(Exception error)
+        {
+            Error = error;
+        }
+    }
+}
