27ab0ec0caaabd5132c273f9841013856f8f3c24
vpp090 <vasil.petrov090@gmail.com>
2024-01-12 13:13:52 +0200

    expansion changes.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ServiceResponseR
 A NuGet package for common service responses with generic placeholder and a property for an error.
+
+Contains: 
+
+ServiceResponse class with properties:
+
+T Data
+ErrorMessage
+ErrorDetails
+ErrorCode
+IsSuccess
+
+There is a separate ErrorDetails class for further information about developers with properties
+DevelopmentMessage
+AdditionalContext
--- ServiceResponse.cs
+++ ServiceResponse.cs
@@ -3,17 +3,28 @@ namespace ServiceResponseR
 {
     public class ServiceResponse<T>
     {
-        public T Data { get; set; }
-        public Exception Error { get; set; }
+        public T Data { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public ErrorDetails ErrorDetails { get; private set; }
+        public string ErrorCode { get; private set; }
+        public bool IsSuccess { get; private set; }
 
         public ServiceResponse(T data)
         {
-            Data = data;
+            Data = data ?? throw new ArgumentException(nameof(data));
         }
 
-        public ServiceResponse(Exception error)
+        public ServiceResponse(string errorCode, string errorMessage, ErrorDetails errorDetails = null)
         {
-            Error = error;
+            ErrorCode = errorCode ?? throw new ArgumentException(nameof(errorCode));
+            ErrorMessage = errorMessage ?? throw new ArgumentException(nameof(errorMessage));
+            ErrorDetails = errorDetails;
         }
+    }
+
+    public class ErrorDetails
+    {
+        public string DeveloperMessage { get; set; }
+        public string AdditionalContext { get; set; }
     }
 }
